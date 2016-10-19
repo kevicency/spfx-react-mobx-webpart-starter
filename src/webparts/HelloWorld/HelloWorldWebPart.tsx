1f8d45d8fd7552928069ca1528a738f87351ec76
Kevin Mees <kev.mees@gmail.com>
2016-10-19 17:45:17 +0200

    Create store in webpart and render redux provider

--- src/webparts/HelloWorld/HelloWorldWebPart.tsx
+++ src/webparts/HelloWorld/HelloWorldWebPart.tsx
@@ -6,24 +6,33 @@ import {
   IWebPartContext,
   PropertyPaneTextField
 } from '@microsoft/sp-webpart-base'
+import { Store } from 'redux'
+import { Provider } from 'react-redux'
 
 import * as strings from 'helloWorldStrings'
-import { HelloWorld, IHelloWorldProps } from '../../components/HelloWorld'
+import { IState} from '../../reducers'
+import configureStore from '../../configureStore'
+import { HelloWorld } from '../../components/HelloWorld'
 
 export interface IHelloWorldWebPartProps {
   name: string
 }
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+  store: Store<IState>
 
   public constructor(context: IWebPartContext) {
     super(context)
+
+    this.store = configureStore({})
   }
 
   public render(): void {
-    const element: React.ReactElement<IHelloWorldProps> = React.createElement(HelloWorld, {
-      name: this.properties.name
-    })
+    const element = (
+      <Provider store={this.store}>
+        <HelloWorld name={this.properties.name} />
+      </Provider>
+    )
 
     ReactDom.render(element, this.domElement)
   }
