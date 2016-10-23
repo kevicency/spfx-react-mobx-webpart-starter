cd932a23917bef61447648ab311f430640edb22a
Kevin Mees <kev.mees@gmail.com>
2016-10-23 15:54:15 +0200

    Fix disbaleReactive property access and add whyRun output

--- src/containers/HelloWorldContainer.tsx
+++ src/containers/HelloWorldContainer.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { observer } from 'mobx-react'
+import { whyRun } from 'mobx'
 
 import Greeter from '../components/Greeter'
 
 const HelloWorldContainer = ({ webpart: { properties } }) => (
   <div>
+    { console.log(whyRun()) }
     <Greeter name={properties.get('name')} />
-    <pre>{ JSON.stringify({ reactive: !properties.reativeDisabled }) }</pre>
+    <pre>{ JSON.stringify({ reactive: !properties.get('disableReactive') }) }</pre>
   </div>
 )
 
